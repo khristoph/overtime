f4471ce56174a9d9948ecd15114828980190fb38
khristoph <christopherfriedman@gmail.com>
2018-08-15 08:23:17 -0500

    Styled show page and edit views."

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "1 User created"
 AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Snow")
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
