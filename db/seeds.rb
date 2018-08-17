3944c268bc88d313e23b3f5719b4a4a5b26a2356
khristoph <christopherfriedman@gmail.com>
2018-08-17 15:06:52 -0500

    Implemented audit log tab and only show to admin users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-@user = User.create(email: "test@test.com",
+@user = User.create!(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon", last_name: "Snow",
                     phone: "1234567890")
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com",
+AdminUser.create!(email: "admin@test.com",
                  password: "asdfasdf",
                  password_confirmation: "asdfasdf",
                  first_name: "Admin",
-                 last_name: "Snow")
+                 last_name: "Snow",
+                 phone:1234567899)
+puts "Admin Created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
