a2633eb3753e413b2f09c6c45d9d3e9d048b354b
khristoph <christopherfriedman@gmail.com>
2018-08-20 15:02:48 -0500

    Updated seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,17 @@ AdminUser.create!(email: "admin@test.com",
                  phone:1234567899)
 puts "Admin Created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices enim urna, quis consectetur nibh dapibus ac. Curabitur augue lorem, semper non sagittis in, commodo mattis dui. Curabitur rhoncus efficitur purus quis scelerisque. In luctus neque eu arcu auctor interdum. Sed tempus condimentum magna, accumsan condimentum nunc maximus eget. Quisque quis nunc non lacus dignissim tempor. Donec faucibus, neque quis blandit interdum, ipsum metus posuere enim, eleifend consequat ipsum ante in nisi. Nunc euismod dolor ut nibh sollicitudin dignissim.", user_id: @user.id, overtime_request: 2.5)
-end
-puts "100 posts have been created"
-
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices enim urna, quis consectetur nibh dapibus ac. Curabitur augue lorem, semper non sagittis in, commodo mattis dui. Curabitur rhoncus efficitur purus quis scelerisque. In luctus neque eu arcu auctor interdum. Sed tempus condimentum magna, accumsan condimentum nunc maximus eget. Quisque quis nunc non lacus dignissim tempor. Donec faucibus, neque quis blandit interdum, ipsum metus posuere enim, eleifend consequat ipsum ante in nisi. Nunc euismod dolor ut nibh sollicitudin dignissim.", user_id: @user.id, overtime_request: 2.5)
+end
+puts "100 posts have been created"
+
+
+
 
