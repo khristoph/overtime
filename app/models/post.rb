6c511583a8a272ead9705d00476c380d5a006171
khristoph <christopherfriedman@gmail.com>
2018-08-23 12:39:33 -0500

    Implemented nil guard for callback.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :date, :rationale, :overtime_request
 
+
   scope :posts_by, ->(user) { where(user_id: user.id) }
+
+  after_save :update_audit_log
 
   private
 
-  # def update_audit_log
-  #   audit_log = AuditLog.where(user_id, start_date: (self.date - 7.days))
-  # end
+  def update_audit_log
+    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+    audit_log.confirmed! if audit_log
+  end
 end
